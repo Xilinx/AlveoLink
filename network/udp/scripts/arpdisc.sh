0fc122b7f25ac1bf5ce6de3ed817240dcaed9992
Lisa Liu <lingl@xilinx.com>
2022-08-18 06:53:03 -0700

    get hivenet ready

--- network/udp/scripts/arpdisc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source /opt/xilinx/xrt/setup.sh
-
-echo "$2/network/udp/sw/build_dir.hw.xilinx_u55c_gen3x16_xdma_2_202110_1/$1/arpDisc.exe $3 $4"
-
-$2/network/udp/sw/build_dir.hw.xilinx_u55c_gen3x16_xdma_2_202110_1/$1/arpDisc.exe $3 $4
-sleep 1
-echo "done for $1"
