8523a88d534665da6a084cce66cadf46d41862f7
Lisa Liu <lingl@xilinx.com>
2022-09-22 02:36:31 -0700

    add idle cycle parameter for tm_managers shell script

--- tests/kernel/sync_dv/tm_managers/run_tm_managers.sh
+++ tests/kernel/sync_dv/tm_managers/run_tm_managers.sh
@@ -5,8 +5,8 @@
 CUR_DIR=$(pwd)
 AL_PATH=$(awk 'sub(/AlveoLink.*/, "")' <<< "$CUR_DIR")AlveoLink
 
-ssh -f xsj-dxgradb01  "cd $AL_PATH/tests/kernel/sync_dv/tm_managers; bash tm_managers.sh xsj-dxgradb01 1"
+ssh -f xsj-dxgradb01  "cd $AL_PATH/tests/kernel/sync_dv/tm_managers; bash tm_managers.sh xsj-dxgradb01 1 $1"
 
-ssh -f xsj-dxgradb03  "cd $AL_PATH/tests/kernel/sync_dv/tm_managers; bash tm_managers.sh xsj-dxgradb03 0"
+ssh -f xsj-dxgradb03  "cd $AL_PATH/tests/kernel/sync_dv/tm_managers; bash tm_managers.sh xsj-dxgradb03 0 $1"
 
 sleep 1
--- tests/kernel/sync_dv/tm_managers/tm_managers.sh
+++ tests/kernel/sync_dv/tm_managers/tm_managers.sh
@@ -2,10 +2,10 @@
 source /proj/xbuilds/2022.1_released/installs/lin64/Vitis/2022.1/settings64.sh
 source /opt/xilinx/xrt/setup.sh
 
-echo "run tm_managers on host $1 device $2"
+echo "run tm_managers on host $1 device $2 with idleCycles $3"
 echo "run cmd: "
-echo "./build_dir.hw.xilinx_u55c_gen3x16_xdma_2_202110_1/$1/manager.exe /proj/rdi-xsj/staff/lingl/nobkup/xclbins/nhop_xnik_dv/traffic_managers.xclbin $2 65536 15" 
+echo "./build_dir.hw.xilinx_u55c_gen3x16_xdma_2_202110_1/$1/manager.exe /proj/rdi-xsj/staff/lingl/nobkup/xclbins/nhop_xnik_dv/traffic_managers.xclbin $2 $3 15" 
 
-./build_dir.hw.xilinx_u55c_gen3x16_xdma_2_202110_1/$1/manager.exe /proj/rdi-xsj/staff/lingl/nobkup/xclbins/nhop_xnik_dv/traffic_managers.xclbin $2 65536 15
+./build_dir.hw.xilinx_u55c_gen3x16_xdma_2_202110_1/$1/manager.exe /proj/rdi-xsj/staff/lingl/nobkup/xclbins/nhop_xnik_dv/traffic_managers.xclbin $2 $3 15
 sleep 1
 echo "done for host $1 device $2"
